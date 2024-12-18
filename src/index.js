cdf815feca6b83204e3fc1143ff5a67ee3285a43
sunnypuppy <wangyuanyuan1024@gmail.com>
2024-12-18 20:58:07 +0800

    recover outbounds type

--- src/index.js
+++ src/index.js
@@ -480,18 +480,20 @@ async function getSingBoxSubConfig(options, nodesFromLinks, nodesFromAddresses, 
 
 	if (domain_outbounds.length > 0) {
 		singboxSubConfig.outbounds.push({
-			type: 'selector',
+			type: 'urltest',
 			tag: '优选域名',
 			outbounds: domain_outbounds.map((outbound) => outbound.tag),
+			interrupt_exist_connections: false,
 		});
 		selector_outbounds.push('优选域名');
 	}
 
 	if (address_outbounds.length > 0) {
 		singboxSubConfig.outbounds.push({
-			type: 'selector',
+			type: 'urltest',
 			tag: '优选IP',
 			outbounds: address_outbounds.map((outbound) => outbound.tag),
+			interrupt_exist_connections: false,
 		});
 		selector_outbounds.push('优选IP');
 	}
