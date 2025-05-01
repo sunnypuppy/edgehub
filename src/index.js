bc60fec2c7d9485cca228272bdd94d76da776f20
sunnypuppy <wangyuanyuan1024@gmail.com>
2025-05-01 13:33:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -426,7 +426,7 @@ async function getSingBoxSubConfig(options, nodesByGroup) {
 			{
 				type: 'tun',
 				tag: 'tun-in',
-				address: '172.18.0.1/30',
+				address: ['172.19.0.1/30', 'fdfe:dcba:9876::1/126'],
 				auto_route: true,
 				strict_route: false,
 			},
